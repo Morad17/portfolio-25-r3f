cf89bfd62d5a0b8d06dd5044650dbb1ff3e8729d
Morad17 <morad123@hotmail.co.uk>
2025-07-01 17:01:49 +0100

    inital commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,8 @@
-import { Canvas } from "@react-three/fiber";
 import React from "react";
-import { Environment } from "@react-three/drei";
-import { VikingShip } from "../components/Viking_Ship";
 
 const Home = () => {
   //Content//
-  return (
-    <div className="home">
-      <Canvas className="home" style={{ width: "100vw", height: "300px" }}>
-        <Environment preset="warehouse" />
-        <ambientLight intensity={0.1} />
-        <spotLight
-          position={[0, 25, 0]}
-          angle={1}
-          castShadow
-          intensity={2}
-          shadow-bias={-0.0001}
-        />
-        <VikingShip scale={0.2} rotation={[0, 2, 0]} position={[2, -1, 2]} />
-      </Canvas>
-    </div>
-  );
+  return <div className="home">Home</div>;
 };
 
 export default Home;
