2a8020d5a11a1abaf7b8b8bf8d93657cb9f6227b
Morad17 <morad123@hotmail.co.uk>
2025-07-04 11:08:17 +0100

    testing scroll controls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
+import { Scroll, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { Environment } from "@react-three/drei";
 import { VikingShip } from "../components/Viking_Ship";
+import Skills from "./Skills";
 
 const Home = () => {
   //Content//
   return (
-    <div className="home">
-      <Canvas className="home" style={{ width: "100vw", height: "300px" }}>
-        <Environment preset="warehouse" />
+    <div className="home-page">
+      <Canvas className="home" style={{ width: "97vw", height: "100vh" }}>
+        <ScrollControls pages={4} damping={0.1}>
+          <VikingShip scale={0.2} rotation={[0, 2, 0]} position={[2, -1, 2]} />
+          <Environment preset="warehouse" />
+          <Scroll html>
+            <h1>My Skills</h1>
+            <Skills />
+          </Scroll>
+        </ScrollControls>
         <ambientLight intensity={0.1} />
         <spotLight
           position={[0, 25, 0]}
@@ -17,7 +26,6 @@ const Home = () => {
           intensity={2}
           shadow-bias={-0.0001}
         />
-        <VikingShip scale={0.2} rotation={[0, 2, 0]} position={[2, -1, 2]} />
       </Canvas>
     </div>
   );
---
+++ src/pages/Skills.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div className="skills-page">
+      <h1 className="skills-title">Skills & Experience</h1>
+      <div className="skills-text">
+        <p className="main-text">
+          From learing to write the first html code, to creating complex react
+          and django e-commerce projects, I have always been keen to always
+          learn new languages. My skills lie in designing a unique and visually
+          aesthetic project and applying the right applications, to achieve the
+          perfect user experience.Whilst having Front-End and Back-End skills, I
+          focus more on Front End. Since 2021 my focus has been mainly on React
+          Libraries.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
